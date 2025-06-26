fcce536520e3ff72843642e21a8e0f21791940f4
Timofey Prokofyev <tryatim8@gmail.com>
2025-06-26 22:42:03 +0400

    add model-admin mapping

--- myproject/tasks/admin.py
+++ myproject/tasks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskModelAdmin(admin.ModelAdmin):
+    """Модель отображения задач в админке."""
+
+    list_display = ['pk', 'title', 'user', 'status', 'description_short']
+    list_display_links = ['pk', 'title', 'status']
+    ordering = ['-pk', 'status']
+
+    @classmethod
+    def description_short(cls, obj: Task):
+        """Короткое описание задачи."""
+
+        if not obj.description:
+            return ''
+        elif len(obj.description) <= 60:
+            return obj.description
+        else:
+            return obj.description[:57] + '...'
