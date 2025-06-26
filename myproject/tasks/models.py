fb3439e0e8d81c4cede7f2abd98d356e127c1ca5
Timofey Prokofyev <tryatim8@gmail.com>
2025-06-26 11:49:42 +0400

    create models

--- myproject/tasks/models.py
+++ myproject/tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Task(models.Model):
+    """Модель задачи."""
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks',
+    )
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=20,
+        default='new',
+        choices=[
+            ('new', 'New'),
+            ('in_progress', 'In progress'),
+            ('completed', 'Completed'),
+        ],
+    )
+
+    def __str__(self):
+        return f'Task `{self.title}` is {self.status}'
