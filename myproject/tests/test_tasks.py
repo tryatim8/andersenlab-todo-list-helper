65035332e21796ffa55fff6a5cc25e59be7e7dda
Timofey Prokofyev <tryatim8@gmail.com>
2025-06-29 18:50:47 +0400

    test delete and mark_completed task

--- myproject/tests/test_tasks.py
+++ myproject/tests/test_tasks.py
@@ -7,7 +7,6 @@ from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
-
 @pytest.mark.django_db
 def test_tasks_list_failed(api_client, tasks_serialized, user):
     response = api_client.get('/api/tasks/')
@@ -42,7 +41,7 @@ def test_tasks_list_with_status_filter(
 
 @pytest.mark.django_db
 def test_task_detail_by_pk_successful(auth_client, tasks_list):
-    tasks_sample = random.choices(tasks_list, k=3)
+    tasks_sample = random.sample(tasks_list, k=3)
     for task in tasks_sample:
         task_pk = task.pk
         response = auth_client.get(f'/api/tasks/{task_pk}/')
@@ -81,7 +80,7 @@ def test_task_full_change_by_id_successful(auth_client, tasks_serialized):
         'description': 'Example text for the task description',
         'status': 'in_progress',
     }
-    tasks_to_change: list[dict] = random.choices(tasks_serialized, k=3)
+    tasks_to_change: list[dict] = random.sample(tasks_serialized, k=3)
     for task in tasks_to_change:
         task_pk = task['pk']
         response = auth_client.put(f'/api/tasks/{task_pk}/', data=response_data)
@@ -102,3 +101,43 @@ def test_task_change_partly_by_id_successful(auth_client, tasks_serialized, titl
     task_to_change.update(response_data)
     expected_task = Task.objects.get(pk=task_pk)
     assert task_to_change == TaskSerializer(expected_task).data
+
+
+@pytest.mark.django_db
+def test_task_delete_by_id_successful(auth_client, tasks_serialized):
+    tasks_sample: list[dict] = random.sample(tasks_serialized, k=3)
+    for task in tasks_sample:
+        task_pk = task['pk']
+        response = auth_client.delete(f'/api/tasks/{task_pk}/')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert response.data is None
+        deleted_task = Task.objects.filter(pk=task_pk)
+        assert list(deleted_task) == []
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('task_pk', [0, -123, 10 ** 10])
+def test_task_delete_by_id_failed(auth_client, tasks_serialized, task_pk):
+    response = auth_client.delete(f'/api/tasks/{task_pk}/')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.data['detail'] == 'No Task matches the given query.'
+
+
+@pytest.mark.django_db
+def test_task_mark_completed_by_id_successful(auth_client, tasks_serialized):
+    tasks_sample: list[dict] = random.sample(tasks_serialized, k=3)
+    for task in tasks_sample:
+        task_pk = task['pk']
+        response = auth_client.post(f'/api/tasks/{task_pk}/mark_completed/')
+        assert response.status_code == status.HTTP_200_OK
+        task.update({'status': 'completed'})
+        expected_task = Task.objects.get(pk=task_pk)
+        assert task == TaskSerializer(expected_task).data == response.data
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('task_pk', [0, -123, 10 ** 10])
+def test_task_mark_completed_by_id_failed(auth_client, tasks_serialized, task_pk):
+    response = auth_client.post(f'/api/tasks/{task_pk}/mark_completed/')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.data['detail'] == 'No Task matches the given query.'
